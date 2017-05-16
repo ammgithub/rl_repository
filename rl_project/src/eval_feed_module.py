6d749090f095487f44f78f6fa4ad362f45619d76
ammgithub <malyscheff@gmail.com>
2017-05-16 14:37:36 -0500

    Test commit.

---
+++ rl_project/src/eval_feed_module.py
@@ -0,0 +1,27 @@
+"""
+Created on May 16, 2017
+
+Evaluative Feedback
+
+Reinforcement Learning, Chapter 2
+(Sutton, Barto, 1998)
+
+"""
+__author__ = 'amm'
+__date__  = "May 16, 2017"
+__version__ = 0.0
+
+import numpy as np
+import pylab as plt
+from copy import deepcopy
+
+np.set_printoptions(linewidth = 100, edgeitems = 'all', suppress = True, 
+                 precision = 4)
+
+    
+if __name__ == '__main__':
+    """
+    execfile('C:\\Users\\amalysch\\git\\rl_repository\\rl_project\\src\\eval_feed_module.py')
+    """
+    print "test"
+
